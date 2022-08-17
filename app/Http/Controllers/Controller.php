781e46339fce6321abe03e5f8c664cc281b66fea
tk.lee <tk.lee@banco.id>
2022-08-17 16:04:56 +0900

    test..

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,22 +6,26 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Http;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function index(){
+    public function index($callback){
 
+        print_r($_GET);
         $url = "https://noblegochang.cafe24api.com/api/v2/oauth/authorize?response_type=code&client_id=RqSe9wsSbaPVz3DzKtt9GA&state=kyun6654&redirect_uri=https://www.gochang-mall.com/api&scope=";
-        $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        // $curl = curl_init($url);
+        // curl_setopt($curl, CURLOPT_URL, $url);
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        $resp = curl_exec($curl);
-        curl_close($curl);
-        var_dump($resp);
-
+        // $resp = curl_exec($curl);
+        // curl_close($curl);
+        // var_dump($resp);
+        $response = Http::withHeaders(['Content-Type' => 'application/json'])->get($url)->json();
+            
+        print_r($response);
     }
     public function test(){
 
@@ -47,6 +51,8 @@ class Controller extends BaseController
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);       //POST data
         curl_setopt($ch, CURLOPT_POST, true);              //true시 post 전송 
 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
         $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $error = curl_error($ch);

02f395d11a870cd696c3c70c229170c5e8ebba2a
kyun <kyun@banco.local>
2022-08-17 09:55:45 +0900

    gitignore 다시 적용

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,6 +14,34 @@ class Controller extends BaseController
 
     public function test(){
 
-        echo 'zzz';
+        $data = array(
+            'name' => '',
+            'email' => 'tk.lee@banco.id',
+            'password' => '',
+            'password_confirmation' => '',
+        );
+        $url = "https://gochang-mall.com/api/auth/register";
+
+        $method = "POST";
+        $ch = curl_init($url);
+        $headers = array(
+            "Content-Type: application/json",
+         );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);        
+        curl_setopt($ch, CURLOPT_URL, $url);               //URL 지정하기
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    //요청 결과를 문자열로 반환 
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);      //connection timeout 10초 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);   //원격 서버의 인증서가 유효한지 검사 안함
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);       //POST data
+        curl_setopt($ch, CURLOPT_POST, true);              //true시 post 전송 
+
+        $response = curl_exec($ch);
+        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $error = curl_error($ch);
+        curl_close($ch);
+
+        print_r($response);exit;
+        return $response;
     }
 }

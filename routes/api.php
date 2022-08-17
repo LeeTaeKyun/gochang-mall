e20b5abfb50c3820a737eb5567a138d197b70d34
kyun <kyun@banco.local>
2022-08-17 11:08:45 +0900

    api route  수정

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\NewPasswordController;
 
 /*
@@ -38,7 +39,7 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
 
 
 
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', [Controller::class, 'test']);
+// Route::middleware('auth:api')->get('/', function (Request $request) {
+//     return $request->user();
+// });
